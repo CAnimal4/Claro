d00a7f615ca25224c35e59796bfb10b61441ff57
CAnimal4 <clarkbythebay@gmail.com>
2026-09-20 16:21:32 -0700

    Improve first-run onboarding guide

--- app.js
+++ app.js
@@ -4094,6 +4094,7 @@ mean/nice
         // first run
         firstRunOverlay: $('firstRunOverlay'),
         firstRunCloseBtn: $('firstRunCloseBtn'),
+        firstRunSkipBtn: $('firstRunSkipBtn'),
         firstRunGotItBtn: $('firstRunGotItBtn')
       };
     },
@@ -4436,6 +4437,7 @@ mean/nice
         this.closeModal(this.$.firstRunOverlay);
       };
       this.$.firstRunGotItBtn.addEventListener('click', closeFirstRun);
+      this.$.firstRunSkipBtn.addEventListener('click', closeFirstRun);
       this.$.firstRunCloseBtn.addEventListener('click', closeFirstRun);
       this.$.firstRunOverlay.addEventListener('click', (e) => {
         if (e.target === this.$.firstRunOverlay) closeFirstRun();
